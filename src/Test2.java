3701ae92744602c219abe2c0dd8a27317d7e2ff6
Erkebaev Nursultan <73390365+Erkebaev95@users.noreply.github.com>
2021-11-21 20:51:59 +0600

    Create Test2.java

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+public class Test2 {
+    public static void main(String[] args) {
+        swap(5, 6);
+        swap(-5, 6);
+        swap(-5, -6);
+    }
+
+    public static void swap(int a, int b) {
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + ":" + b);
+    }
+}
